1bef99f9de41a392b093c40c5860b0352fc86412
Parth Patel <parth.patel1@ucalgary.ca>
2024-03-10 00:35:24 -0700

    initial components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./horizontal_prototpye/**/*.{html,js}"],
   theme: {
     extend: {},
   },
